66bf47a5047ac037b5f4e034a711529265e1a2c3
Nick Butcher <NickButcher1@users.noreply.github.com>
2022-12-18 17:48:25 +0000

    Day 4

---
+++ src/day04.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+from abstractsolver import AbstractSolver
+
+
+class Solver(AbstractSolver):
+    input_lines: list
+
+    def read_input(self, lines: list) -> None:
+        # TODO: Process the input.
+        self.input_lines = lines
+
+    def solve1(self) -> int:
+        score = 0
+        for line in self.input_lines:
+            split_line = line.split(",")
+            one_start = int(split_line[0].split("-")[0])
+            one_end = int(split_line[0].split("-")[1])
+            two_start = int(split_line[1].split("-")[0])
+            two_end = int(split_line[1].split("-")[1])
+
+            if one_start >= two_start and one_end <= two_end:
+                score += 1
+            elif two_start >= one_start and two_end <= one_end:
+                score += 1
+
+        return score
+
+    def solve2(self) -> int:
+        score = 0
+        for line in self.input_lines:
+            split_line = line.split(",")
+            one_start = int(split_line[0].split("-")[0])
+            one_end = int(split_line[0].split("-")[1])
+            two_start = int(split_line[1].split("-")[0])
+            two_end = int(split_line[1].split("-")[1])
+
+            if one_start < two_start:
+                if one_end >= two_start:
+                    score += 1
+            elif one_start > two_start:
+                if two_end >= one_start:
+                    score += 1
+            else:
+                score += 1
+
+        return score
