a4fa2cf3a6e848e8ea9b666e4efb5ac9b927b3d2
Nick Butcher <NickButcher1@users.noreply.github.com>
2022-12-18 18:41:45 +0000

    Day 6

---
+++ src/day06.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+from abstractsolver import AbstractSolver
+
+
+class Solver(AbstractSolver):
+    input_lines: list
+
+    def is_unique(self, input):
+        dict1 = {}
+        for i in range(0, len(input)):
+            if input[i] in dict1.keys():
+                return False
+            else:
+                dict1[input[i]] = 1
+        return True
+
+    def read_input(self, lines: list) -> None:
+        self.input_lines = lines
+
+    def solve1(self):
+        for line in self.input_lines:
+            for i in range(0, len(line)):
+                if self.is_unique(line[i : i + 4]):
+                    return i + 4
+
+    def solve2(self):
+        for line in self.input_lines:
+            for i in range(0, len(line)):
+                if self.is_unique(line[i : i + 14]):
+                    return i + 14
