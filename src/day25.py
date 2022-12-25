f88415a550123b4d2a5b7213f3a6e4a0d2f800cd
Nick Butcher <NickButcher1@users.noreply.github.com>
2022-12-25 08:33:02 +0000

    Day 25

--- src/day25.py
+++ src/day25.py
@@ -2,13 +2,48 @@
 
 from abstractsolver import AbstractSolver
 
+INT_TO_SNAFU = {-2: "=", -1: "-", 0: "0", 1: "1", 2: "2"}
+SNAFU_TO_INT = {"=": -2, "-": -1, "0": -1, "1": 1, "2": 2}
+
 
 class Solver(AbstractSolver):
     def read_input(self) -> None:
         pass
 
-    def solve1(self):
-        return -1
+    def decimal_to_snafu(self, sum: int) -> str:
+        reverse_output = ""
+        remaining_sum = sum
 
+        while remaining_sum != 0:
+            remainder = remaining_sum % 5
+            remaining_sum = int(remaining_sum / 5)
+
+            if remainder == 3:
+                remainder = -2
+                remaining_sum += 1
+            elif remainder == 4:
+                remainder = -1
+                remaining_sum += 1
+            reverse_output += INT_TO_SNAFU[remainder]
+        return reverse_output[::-1]
+
+    def snafu_to_decimal(self, snafu_str) -> int:
+        mult = 1
+        this_sum = 0
+        for snafu_c in snafu_str[::-1]:
+            this_sum += SNAFU_TO_INT[snafu_c] * mult
+            mult *= 5
+
+        return this_sum
+
+    def solve1(self):
+        sum = 0
+        for line in self.input_lines:
+            this_sum = 0
+            snafu_str = line.split()[0]
+            sum += self.snafu_to_decimal(snafu_str)
+        return self.decimal_to_snafu(sum)
+
+    # There is no part two.
     def solve2(self):
         return -1
