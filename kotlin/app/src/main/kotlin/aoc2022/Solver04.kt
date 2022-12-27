61681f97d04fd7e22ac8bc05827d8ff1bf2992e1
Nick Butcher <NickButcher1@users.noreply.github.com>
2022-12-27 08:15:53 +0000

    Kotlin: Day 4

--- kotlin/app/src/main/kotlin/aoc2022/Solver04.kt
+++ kotlin/app/src/main/kotlin/aoc2022/Solver04.kt
@@ -4,11 +4,43 @@ class Solver04(
     inputLines: List<String>,
     isPartTwo: Boolean = false
 ) : Solver(inputLines, isPartTwo) {
+    private fun solveCommon(): Int {
+        var score = 0
+        inputLines.forEach { line ->
+            val splitLine = line.split(",")
+            val oneStart = splitLine[0].split("-")[0].toInt()
+            val oneEnd = splitLine[0].split("-")[1].toInt()
+            val twoStart = splitLine[1].split("-")[0].toInt()
+            val twoEnd = splitLine[1].split("-")[1].toInt()
+
+            if (isPartTwo) {
+                if (oneStart < twoStart) {
+                    if (oneEnd >= twoStart) {
+                        score += 1
+                    }
+                } else if (oneStart > twoStart) {
+                    if (twoEnd >= oneStart) {
+                        score += 1
+                    }
+                } else {
+                    score += 1
+                }
+            } else {
+                if (oneStart >= twoStart && oneEnd <= twoEnd) {
+                    score += 1
+                } else if (twoStart >= oneStart && twoEnd <= oneEnd) {
+                    score += 1
+                }
+            }
+        }
+        return score
+    }
+
     override fun solve1(): Any {
-        return "-1"
+        return solveCommon()
     }
 
     override fun solve2(): Any {
-        return "-1"
+        return solveCommon()
     }
 }
--- python/src/day04.py
+++ python/src/day04.py
@@ -7,7 +7,7 @@ class Solver(AbstractSolver):
     def read_input(self) -> None:
         pass
 
-    def solve1(self):
+    def solve_common(self):
         score = 0
         for line in self.input_lines:
             split_line = line.split(",")
@@ -16,29 +16,25 @@ class Solver(AbstractSolver):
             two_start = int(split_line[1].split("-")[0])
             two_end = int(split_line[1].split("-")[1])
 
-            if one_start >= two_start and one_end <= two_end:
-                score += 1
-            elif two_start >= one_start and two_end <= one_end:
-                score += 1
-
-        return score
-
-    def solve2(self):
-        score = 0
-        for line in self.input_lines:
-            split_line = line.split(",")
-            one_start = int(split_line[0].split("-")[0])
-            one_end = int(split_line[0].split("-")[1])
-            two_start = int(split_line[1].split("-")[0])
-            two_end = int(split_line[1].split("-")[1])
-
-            if one_start < two_start:
-                if one_end >= two_start:
-                    score += 1
-            elif one_start > two_start:
-                if two_end >= one_start:
+            if self.is_part_two:
+                if one_start < two_start:
+                    if one_end >= two_start:
+                        score += 1
+                elif one_start > two_start:
+                    if two_end >= one_start:
+                        score += 1
+                else:
                     score += 1
             else:
-                score += 1
+                if one_start >= two_start and one_end <= two_end:
+                    score += 1
+                elif two_start >= one_start and two_end <= one_end:
+                    score += 1
 
         return score
+
+    def solve1(self):
+        return self.solve_common()
+
+    def solve2(self):
+        return self.solve_common()
