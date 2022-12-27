4a19cda26c04b968340e15cc3145d98639c112a2
Nick Butcher <NickButcher1@users.noreply.github.com>
2022-12-27 14:25:16 +0000

    Kotlin: Day 25

--- kotlin/app/src/main/kotlin/aoc2022/Solver25.kt
+++ kotlin/app/src/main/kotlin/aoc2022/Solver25.kt
@@ -4,11 +4,67 @@ class Solver25(
     inputLines: List<String>,
     isPartTwo: Boolean = false
 ) : Solver(inputLines, isPartTwo) {
+    private fun decimalToSnafu(decimal: Long): String {
+        var reverseOutput = ""
+        var remainingDecimal = decimal
+
+        while (remainingDecimal != 0L) {
+            var remainder = remainingDecimal % 5
+            remainingDecimal = (remainingDecimal / 5)
+
+            if (remainder == 3L) {
+                remainder = -2L
+                remainingDecimal += 1
+            } else if (remainder == 4L) {
+                remainder = -1L
+                remainingDecimal += 1
+            }
+            reverseOutput += LONG_TO_SNAFU[remainder]
+        }
+
+        return reverseOutput.reversed()
+    }
+
+    private fun snafuToDecimal(snafu: String): Long {
+        var mult = 1L
+        var sum = 0L
+
+        snafu.reversed().forEach { snafuChar ->
+            sum += SNAFU_TO_LONG[snafuChar]!! * mult
+            mult *= 5
+        }
+        return sum
+    }
+
     override fun solve1(): Any {
+        var sum = 0L
+        inputLines.forEach { line ->
+            sum += snafuToDecimal(line.split(" ")[0])
+        }
+
+        return decimalToSnafu(sum)
+    }
+
+    /** There is no part two. */
+    override fun solve2(): Any {
         return "-1"
     }
 
-    override fun solve2(): Any {
-        return "-1"
+    companion object {
+        private val LONG_TO_SNAFU = mapOf(
+            -2L to "=",
+            -1L to "-",
+            0L to "0",
+            1L to "1",
+            2L to "2"
+        )
+
+        private val SNAFU_TO_LONG = mapOf(
+            '=' to -2L,
+            '-' to -1L,
+            '0' to 0L,
+            '1' to 1L,
+            '2' to 2L
+        )
     }
 }
--- python/src/day25.py
+++ python/src/day25.py
@@ -10,31 +10,30 @@ class Solver(AbstractSolver):
     def read_input(self) -> None:
         pass
 
-    def decimal_to_snafu(self, sum: int) -> str:
+    def decimal_to_snafu(self, decimal: int) -> str:
         reverse_output = ""
-        remaining_sum = sum
+        remaining_decimal = decimal
 
-        while remaining_sum != 0:
-            remainder = remaining_sum % 5
-            remaining_sum = int(remaining_sum / 5)
+        while remaining_decimal != 0:
+            remainder = remaining_decimal % 5
+            remaining_decimal = int(remaining_decimal / 5)
 
             if remainder == 3:
                 remainder = -2
-                remaining_sum += 1
+                remaining_decimal += 1
             elif remainder == 4:
                 remainder = -1
-                remaining_sum += 1
+                remaining_decimal += 1
             reverse_output += INT_TO_SNAFU[remainder]
         return reverse_output[::-1]
 
     def snafu_to_decimal(self, snafu_str) -> int:
         mult = 1
-        this_sum = 0
+        sum = 0
         for snafu_c in snafu_str[::-1]:
-            this_sum += SNAFU_TO_INT[snafu_c] * mult
+            sum += SNAFU_TO_INT[snafu_c] * mult
             mult *= 5
-
-        return this_sum
+        return sum
 
     def solve1(self):
         sum = 0
